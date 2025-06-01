8ab53062bbe87b980bab832baadbf1f033d9d9f4
Markost1 <markostevovic565@gmail.com>
2025-06-01 15:39:07 +0200

    Kreirane stranice i rute, Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/signIn"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
 
 export default function App() {
   return (
-    <div className='text-red-300'>App</div>
+    <BrowserRouter>
+    <Routes >
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/signup" element={<SignUp />}/>
+      <Route path="/signin" element={<SignIn />}/>
+      <Route path="/profile" element={<Profile />}/>
+    </Routes>
+    </BrowserRouter>
+ 
   )
 }
