0b09010c33b598f9ad3019a36251ddd81d921ce4
Markost1 <markostevovic565@gmail.com>
2025-06-01 18:49:06 +0200

    Konektovanje na MongoDB podesavanja, sakrivanje mongodDB stringa env

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
-
 import express from 'express'
+import mongoose from 'mongoose';
+import 'dotenv/config'
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to mongoDB');
+    
+}).catch((err)=>{
+    console.log(err);
+    
+})
 
 const app = express()
 
 app.listen(3000,()=>{
-    console.log('Server is listennig on port 3000');
+    console.log('Server is listennig on port 3000!');
     
 })
+
