ca8f8b099721a259ce9c59150d4ac64c488e6a80
Markost1 <markostevovic565@gmail.com>
2025-06-03 21:05:05 +0200

    kreiran error midleware i funkcija za rucno upravljanje greskama

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { handleError } from "../utils/error.js";
 
 
-export const signUp = async (req,res) =>{
+export const signUp = async (req,res,next) =>{
     const {username,email,password} = req.body;
     const hashedPassword = bcrypt.hashSync(password,10);
 
@@ -12,6 +13,8 @@ export const signUp = async (req,res) =>{
         await newUser.save();
         res.status(200).json({message:'User is successfully saved in DB'})
     } catch (error) {
-        res.status(500).json(error.message)
+        //res.status(500).json(error.message)
+        //next(error) prosledjujem error
+        return next(handleError(500,'i made this error manually'))
     }
 }
