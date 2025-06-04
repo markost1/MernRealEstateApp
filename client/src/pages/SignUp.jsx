0a0251ed2b1d66b4c8f1b9f90c3d725fb51841f8
Markost1 <markostevovic565@gmail.com>
2025-06-04 20:12:51 +0200

    useState formData

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+const [formData,setFormData] = useState({});
+
+const handleChange = (e) =>{
+  setFormData({
+    ...formData,
+    [e.target.id] : e.target.value
+  })
+}
+
+console.log(formData);
+
+
+
   return (
     <div className='p-3 max-w-xl mx-auto'>
       <h1 className='my-7 text-center text-3xl  font-semibold '>Sign Up Page</h1>
       <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='Username' id="username" className=' p-3 border rounded-lg'/>
-        <input type='email' placeholder='Email' id="email" className=' p-3 border rounded-lg'/>
-        <input type='password' placeholder='Password' id="password" className=' p-3 border rounded-lg'/>
+        <input type='text' placeholder='Username' id="username" className=' p-3 border rounded-lg' onChange={handleChange}/>
+        <input type='email' placeholder='Email' id="email" className=' p-3 border rounded-lg' onChange={handleChange}/>
+        <input type='password' placeholder='Password' id="password" className=' p-3 border rounded-lg' onChange={handleChange}/>
         <button className='p-3 bg-blue-700 text-white  border rounded-lg uppercase hover:opacity-90 disabled:opacity-85'>Sign up</button>
       </form>
       <div className='flex gap-2 my-5'>
