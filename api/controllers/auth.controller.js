c1e5c1009939b7b9507772774d5f37d18a76150b
Markost1 <markostevovic565@gmail.com>
2025-06-06 18:57:53 +0200

    Zavrsena SignIN  funkcija, kreiran jwt, cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import jwt from 'jsonwebtoken';
 import { handleError } from "../utils/error.js";
 
 
@@ -19,6 +20,31 @@ export const signUp = async (req,res,next) =>{
     }
 }
 
-export const signIn = (req,res,next) => {
-    res.status(200).json({message :'Sign in ruta je formirana'})
+export const signIn = async (req,res,next) => {
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(handleError(400,'User not found'))
+        
+        const validPassword =await bcrypt.compare(password, validUser.password) //povećava skalabilnost servera jer ne blokira event loop
+        if(!validPassword) return next(handleError(404,'Wrong Credential'))
+
+        const token = jwt.sign({id:validUser._id},
+                                process.env.TOKEN,
+                                { expiresIn: '7d',})
+
+        const {password:_, ...rest} = validUser._doc
+
+        return res.cookie('access_token', token, {
+            httpOnly:true,
+            sameSite: 'strict',
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)  
+            }).status(200).json({rest})
+
+    } catch (error) {
+        console.log(error);
+        return next(handleError(500, 'Server error'));
+        
+    }
+    
 }
