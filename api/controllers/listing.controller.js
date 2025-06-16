78ac089704ea81da7c3bd3420884c483c29c369f
Markost1 <markostevovic565@gmail.com>
2025-06-16 15:05:42 +0200

    yavrsio listing model, create listing funkciju u controlleru, mali bug rijesen prilikom cuvanja listinga...

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,16 @@
-export const createListing = (req,res,next) =>{
-    return res.status(200).json('radi')
+import Listing from "../models/listing.model.js";
+
+export const createListing = async (req,res,next) =>{
+   
+   const newListing = await Listing.create(req.body)
+    try {
+        await newListing.save();
+        res.status(200).json({message: 'Listing is successfully saved in DB '})
+
+        
+    } catch (error) {
+       console.log(error);
+       
+    }
     
 }
