3a2b40061481b666956c49f4530f31308c43a1f6
Markost1 <markostevovic565@gmail.com>
2025-07-03 23:02:12 +0200

    Koristio sam useEffect da bih prilikom ucitavanja Home page fetchovao listinge i prikazao ih

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useState } from "react"
+import { useEffect } from "react"
+
 
 export default function Home() {
+const [showListings, setShowListings] = useState({})
+
+useEffect(()=>{
+
+const fetchData = async() =>{
+  
+  //console.log('funkcija radi');
+  try {
+  const res = await fetch('/api/listing/get?limit=100',{
+      method:'GET'
+  })
+  
+  const data = await res.json()
+  
+  if (data.success === false) {
+      console.log(data.message);
+    }
+    
+    setShowListings(data.listings)
+    
+    
+  } catch (error) {
+    console.log(error.message);
+    
+  }
+}
+
+
+fetchData()
+
+},[])
+
+useEffect(()=>{
+  console.log('podaci su ucitani',showListings);
+  
+},[showListings])
+
   return (
-    <div>Home</div>
+    <div>
+      {showListings && showListings.length > 0 ? showListings.map((listing)=>{
+       return <p key={listing._id}>{listing._id}</p>
+      }) : null }
+    </div>
   )
 }
