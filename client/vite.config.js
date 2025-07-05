2b3f0317a6871db0367e420b4d5c10154e6d9a7d
Markost1 <markostevovic565@gmail.com>
2025-07-05 22:34:29 +0200

    Omogucio pristup app sa telefona

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   server:{
+  host:true,
   proxy:{
     '/api':{
       target:'http://localhost:3000',
-      secure:false
+      secure:false,
     },
   },
 },
